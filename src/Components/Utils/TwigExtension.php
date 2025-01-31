4d0de9ff34b436f26aa6a84a9fc20f02b3057f25
Constantin <constantin@codiverse.de>
2025-01-31 10:40:20 +0100

    Trim Leerzeichen

--- src/Components/Utils/TwigExtension.php
+++ src/Components/Utils/TwigExtension.php
@@ -42,7 +42,7 @@ class TwigExtension extends AbstractExtension
     {
         if(empty($containerIds) || $containerIds == '') return [];
 
-        return explode(',', $containerIds);
+        return array_map('trim', explode(',', $containerIds));
     }
 
     /**
