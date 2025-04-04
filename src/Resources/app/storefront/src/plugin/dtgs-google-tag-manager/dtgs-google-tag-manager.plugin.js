c7cbc3defc2af6a7d5b7eba9406ec2d210d1b872
Constantin <constantin@codiverse.de>
2025-04-04 14:38:20 +0200

    Change consent variable in JS script
    Use consent variable from analytics.twig also in updateConsentMethod

--- src/Resources/app/storefront/src/plugin/dtgs-google-tag-manager/dtgs-google-tag-manager.plugin.js
+++ src/Resources/app/storefront/src/plugin/dtgs-google-tag-manager/dtgs-google-tag-manager.plugin.js
@@ -11,7 +11,9 @@ export default class DtgsGoogleTagManagerPlugin extends Plugin
 {
     init() {
         this.cookieEnabledName = 'dtgsAllowGtmTracking';
-        this.cookieAdsEnabledName = 'google-ads-enabled';
+        this.cookieAdsEnabledName = window.googleAdsCookieName || 'google-ads-enabled';
+
+        alert(this.cookieAdsEnabledName);
 
         this.handleCookieChangeEvent();
 
