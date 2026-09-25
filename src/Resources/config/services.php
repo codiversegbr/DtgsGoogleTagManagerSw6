90f9803a414e9155ccaf7ca7321992963700aac6
Constantin <constantin@codiverse.de>
2026-09-25 13:19:22 +0200

    remove old snippet file calls, use autoload

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -4,8 +4,6 @@ use Dtgs\GoogleTagManager\Components\Utils\TwigExtension;
 use Dtgs\GoogleTagManager\Core\Content\DtgsGtmCustomService\CustomServiceDefinition;
 use Dtgs\GoogleTagManager\Core\Content\DtgsGtmCustomService\Translation\ServiceTranslationDefinition;
 use Dtgs\GoogleTagManager\Framework\Cookie\CustomCookieProvider;
-use Dtgs\GoogleTagManager\Resources\snippet\de_DE\SnippetFile_de_DE;
-use Dtgs\GoogleTagManager\Resources\snippet\en_GB\SnippetFile_en_GB;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Shopware\Storefront\Framework\Cookie\CookieProviderInterface;
 
@@ -35,13 +33,4 @@ return static function (Symfony\Component\DependencyInjection\Loader\Configurato
     // Twig Function
     $services->set(TwigExtension::class)
         ->tag('twig.extension');
-
-    // Snippets
-    $services->set(SnippetFile_en_GB::class)
-        ->public()
-        ->tag('shopware.snippet.file', ['priority' => 100]);
-
-    $services->set(SnippetFile_de_DE::class)
-        ->public()
-        ->tag('shopware.snippet.file', ['priority' => 100]);
 };
